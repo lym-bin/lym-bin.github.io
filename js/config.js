e872f06a52e3c8467e7f5e75a617e3933e0c0a9b
lym-bin <ushersoul@gmail.com>
2026-08-21 11:05:03 +0900

    fix: config.js 추가 및 배포 복구

---
+++ js/config.js
@@ -0,0 +1,15 @@
+// 공공데이터포털 일반 인증키 (Decoding)
+const API_KEY =
+  "e3de32b504186b1bd67708d74352fbc9b6e6c31af7d60943438978c5693b8d70";
+// 통계 API End Point
+const STATS_API_URL =
+  "https://apis.data.go.kr/1543061/rescueAnimalStatsService/rescueAnimalStats";
+// 시군구 API End Point
+const SiGUNGU_API_URL =
+  "https://apis.data.go.kr/1543061/rescueAnimalStatsService/rescueAnimalSigungu";
+// 유기동물 조회 API End point
+const RESCUEANIMAL_API_URL =
+  "https://apis.data.go.kr/1543061/abandonmentPublicService_v2/abandonmentPublic_v2";
+// 유기동물 보호소 조회 API End point
+const SHELTER_API_URL =
+  "https://apis.data.go.kr/1543061/abandonmentPublicService_v2/shelter_v2";
