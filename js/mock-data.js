05be2b5e1d6bb3bc3f9fc7821bd71f7512829054
lym-bin <ushersoul@gmail.com>
2026-09-10 12:51:20 +0900

    perf: 사진 이미지 리사이즈 + JPG 변환 (images 40MB → 2.4MB)

--- js/mock-data.js
+++ js/mock-data.js
@@ -6,10 +6,10 @@
 
 // 베스트 입양후기 (main.js)
 const ADOPTION_REVIEWS = [
-  { name: "초코", afterImg: "images/dog_2.png", beforeImg: "images/dog_1.png" },
-  { name: "구름", afterImg: "images/dog_4.png", beforeImg: "images/dog_3.png" },
-  { name: "쵸파", afterImg: "images/dog_6.png", beforeImg: "images/dog_5.png" },
-  { name: "가을", afterImg: "images/cat_2.png", beforeImg: "images/cat_1.png" },
+  { name: "초코", afterImg: "images/dog_2.jpg", beforeImg: "images/dog_1.jpg" },
+  { name: "구름", afterImg: "images/dog_4.jpg", beforeImg: "images/dog_3.jpg" },
+  { name: "쵸파", afterImg: "images/dog_6.jpg", beforeImg: "images/dog_5.jpg" },
+  { name: "가을", afterImg: "images/cat_2.jpg", beforeImg: "images/cat_1.jpg" },
 ];
 
 // 기부 챌린지 (main.js)
@@ -19,29 +19,29 @@ const DONATION_CHALLENGES = [
     desc: "더위에 힘든 아이들에게 용품을",
     current: 3200000,
     goal: 5000000,
-    image: "images/donation_1.png",
+    image: "images/donation_1.jpg",
   },
   {
     title: "중성화 수술 지원",
     desc: "유기를 막기 위한 필수 수술비",
     current: 1800000,
     goal: 5000000,
-    image: "images/donation_2.png",
+    image: "images/donation_2.jpg",
   },
   {
     title: "긴급 치료비 기금",
     desc: "다치고 아픈 아이들의 수술비 모금",
     current: 4100000,
     goal: 5000000,
-    image: "images/donation_3.png",
+    image: "images/donation_3.jpg",
   },
 ];
 
 // 실종/제보 (missing.js)
 const MISSING_REPORTS = [
-  { id: 1, type: "실종", region: "가평군", breed: "포메라니안", color: "갈색/흰색", image: "images/missing_1.png" },
-  { id: 2, type: "제보", region: "마포구", breed: "강아지", color: "흰색", image: "images/missing_2.png" },
-  { id: 3, type: "실종", region: "구로구", breed: "말티즈", color: "흰색", image: "images/missing_3.png" },
-  { id: 4, type: "실종", region: "수원특례시", breed: "고양이", color: "고등어색", image: "images/missing_4.png" },
-  { id: 5, type: "실종", region: "부산광역시", breed: "웰시코기", color: "흰색/갈색", image: "images/missing_5.png" },
+  { id: 1, type: "실종", region: "가평군", breed: "포메라니안", color: "갈색/흰색", image: "images/missing_1.jpg" },
+  { id: 2, type: "제보", region: "마포구", breed: "강아지", color: "흰색", image: "images/missing_2.jpg" },
+  { id: 3, type: "실종", region: "구로구", breed: "말티즈", color: "흰색", image: "images/missing_3.jpg" },
+  { id: 4, type: "실종", region: "수원특례시", breed: "고양이", color: "고등어색", image: "images/missing_4.jpg" },
+  { id: 5, type: "실종", region: "부산광역시", breed: "웰시코기", color: "흰색/갈색", image: "images/missing_5.jpg" },
 ];
